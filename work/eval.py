e080d284e7863a2798a81606b4de884ef43a508c
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2025-06-08 15:39:01 +0200

    feat: update eval code
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- work/eval.py
+++ work/eval.py
@@ -12,22 +12,26 @@ import torch.multiprocessing as mp
 from torchinfo import summary
 from rich import print
 import time
-
+from datetime import datetime
+DEBUG = False
 USE_CUDA = torch.cuda.is_available()
 # USE_CUDA = False
-
+train_samples = 8
+valid_samples = 2
+num_workers = 0
+validation_workers = num_workers
 # size scale range
-min_scale = 0.95
-max_scale = 1.05
+min_scale = 0.75
+max_scale = 1.25
 
 # rotation range (-angle_range, angle_range)
-angle_range = 5 # degrees
+angle_range = 15 # degrees
 
 # projective variables (p7, p8)
 projective_range = 0
 
 # translation (p3, p6)
-translation_range = 5 # pixels
+translation_range = 10 # pixels
 
 # possible segment sizes
 lower_sz = 200 # pixels, square
@@ -51,8 +55,8 @@ if __name__ == "__main__":
 	parser.add_argument("MODE")
 	parser.add_argument("FOLDER_NAME")
 	parser.add_argument("DATAPATH")
-	parser.add_argument("MODEL_PATH")
-	parser.add_argument("VGG_MODEL_PATH")
+	parser.add_argument("--MODEL_DIRECTORY", type=str, default="checkpoints", help="Directory to save model checkpoints")
+	parser.add_argument("--VGG_MODEL_PATH", type=str, default='x', help="Path to VGG model weights, or 'x' to skip VGG initialization")
 	parser.add_argument("-t","--TEST_DATA_SAVE_PATH")
 	parser.add_argument("--device", type=int, default=0, help="CUDA device number to use (default: 0)")
 	args = parser.parse_args()
@@ -61,7 +65,8 @@ if __name__ == "__main__":
 	FOLDER_NAME = args.FOLDER_NAME
 	FOLDER = FOLDER_NAME + '/'
 	DATAPATH = args.DATAPATH
-	MODEL_PATH = args.MODEL_PATH
+	MODEL_PATH = args.MODEL_DIRECTORY
+	os.makedirs(MODEL_PATH, exist_ok=True)
 	VGG_MODEL_PATH = args.VGG_MODEL_PATH
 	DEVICE = args.device
 	if VGG_MODEL_PATH == 'x':
@@ -82,11 +87,14 @@ def train():
 
     # Initialize model
     dlk_net = dlk.DeepLK(dlk.vgg16Conv(VGG_MODEL_PATH)).to(device)
+    run["model/architecture"] = str(dlk_net)
+    run["model/parameters"] = sum(p.numel() for p in dlk_net.parameters())
     # summary(dlk_net, input_size=[(1, 3, 128, 128), (1, 3, 128, 128)])
 
     lr = 0.0001
-    num_epoch = 10
-    batch_size = 8
+    num_epoch = 100
+    batch_size = 2
+    gradiend_cliping_norm = 0.5
     optimizer = optim.Adam(filter(lambda p: p.requires_grad, dlk_net.parameters()), lr=lr)
     run["parameters"] = {
     "min_scale": min_scale,
@@ -99,6 +107,7 @@ def train():
     "lr": lr,
     "epochs": num_epoch,
     "batch_size" : batch_size,
+    "gradient_clipping_norm": gradiend_cliping_norm,
     }
     # Dataset and DataLoader setup
     param_ranges = {
@@ -118,7 +127,7 @@ def train():
         training_sz=training_sz,
         training_sz_pad=training_sz_pad,
         param_ranges=param_ranges,
-        num_samples =10000,
+        num_samples =train_samples,
         transform=transform,
         device=device
     )
@@ -128,15 +137,17 @@ def train():
         training_sz=training_sz,
         training_sz_pad=training_sz_pad,
         param_ranges=param_ranges,
-        num_samples =64,
+        num_samples =valid_samples,
         transform=transform,
         device=device
     )
 
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
-    # train loader size
-    print(len(dataset))
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=validation_workers)
+    run["parameters/train_dataset_size"] = len(dataset)
+    run["parameters/train_workers"] = num_workers
+    run["parameters/valid_dataset_size"] = len(valid_dataset)
+    run["parameters/valid_workers"] = validation_workers
     best_valid_loss = float('inf')
 
     print('Training...')
@@ -144,7 +155,7 @@ def train():
         dlk_net.train()
         epoch_loss = 0.0
         for batch_idx, (img_batch, template_batch, param_batch) in enumerate(train_loader):
-            start_time = time.time()
+            # start_time = time.time()
             optimizer.zero_grad()
             img_batch = img_batch.to(device)
             template_batch = template_batch.to(device)
@@ -153,13 +164,13 @@ def train():
             img_batch = dlk.normalize_img_batch(img_batch)
             template_batch = dlk.normalize_img_batch(template_batch)
 
-            pred_params, _ = dlk_net(img_batch, template_batch, tol=1e-3, max_itr=1, conv_flag=1)
+            pred_params, _ = dlk_net(img_batch, template_batch, tol=1e-3, max_itr=50, conv_flag=1)
             loss = dlk.corner_loss(pred_params, param_batch, training_sz_pad)
             norm_loss = loss.item()/ float(batch_size)
             run["train/loss"].log(norm_loss)
             epoch_loss += norm_loss
             loss.backward()
-            
+            torch.nn.utils.clip_grad_norm_(dlk_net.parameters(), max_norm=gradiend_cliping_norm)  # Gradient clipping
             optimizer.step()
             
             total_weight_norm = 0.0
@@ -168,9 +179,9 @@ def train():
                 total_weight_norm += param_norm.item() ** 2
             total_weight_norm = total_weight_norm ** 0.5
             run["train/weight_norm"].log(total_weight_norm)
-            elapsed = time.time() - start_time
-            run["train/time_per_batch"].log(elapsed)
-            if batch_idx % batch_size == 0:
+            # elapsed = time.time() - start_time
+            # run["train/time_per_batch"].log(elapsed)
+            if batch_idx % (batch_size)== 0 and DEBUG:
                 log_batch_loss(epoch, batch_idx, norm_loss)
 
     
@@ -178,6 +189,8 @@ def train():
         run["train/epoch_loss"].log(avg_epoch_loss)
         log_epoch_loss(epoch, avg_epoch_loss)
         # Validation
+        # should i empty cache here?
+        # torch.cuda.empty_cache()
         dlk_net.eval()
         total_val_loss = 0
         with torch.no_grad():
@@ -191,7 +204,7 @@ def train():
 
                 pred_params, _ = dlk_net(img_batch, template_batch, tol=1e-3, max_itr=1, conv_flag=1)
                 val_loss = dlk.corner_loss(pred_params, param_batch, training_sz_pad)
-                total_val_loss += val_loss.item()
+                total_val_loss += (val_loss.item()/ float(batch_size))
 
         avg_val_loss = total_val_loss / len(valid_loader)
         run["val/loss"].log(avg_val_loss)
@@ -200,9 +213,11 @@ def train():
 
         if avg_val_loss < best_valid_loss:
             best_valid_loss = avg_val_loss
-            torch.save(dlk_net.conv_func, MODEL_PATH)
-            print("New best model saved.")
-
+            run_name = run["sys/id"].fetch()
+            filename = f"dlk_weights_{run_name}.pt"
+            filepath = os.path.join(MODEL_PATH, filename)
+            torch.save(dlk_net.conv_func, filepath)
+            print(f"New best model saved as {filepath}")
         gc.collect()
         
         
