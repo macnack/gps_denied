190412e26858a8a48b2d043d91091538762aa659
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2025-07-04 01:04:30 +0200

    feat: updated docker
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- docker_ew/run.sh
+++ docker_ew/run.sh
@@ -2,8 +2,8 @@ docker run -it \
     --ipc=host \
     --gpus all \
     --env="XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \
-    --volume="$(pwd)/..:/home/user/work" \
+    --volume="$(pwd)/..:/home/maciek/workspace" \
     --privileged \
     --network=host \
-    --name=gps_denied_new \
-    uav10
+    --name=gps_denied_proj \
+    uav
