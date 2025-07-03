2106fe5cd6dffb52b4d35d5dd8a1ca80e43381ce
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2025-07-04 01:05:43 +0200

    chore: rename folder, rm unused
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,9 @@
 docker run -it \
+    --ipc=host \
+    --gpus all \
     --env="XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \
-    --volume="$(pwd)/..:/home/user/work" \
+    --volume="$(pwd)/..:/home/maciek/workspace" \
     --privileged \
     --network=host \
-    --name=gps_denied \
+    --name=gps_denied_proj \
     uav
--- docker_ew/run.sh
+++
@@ -1,9 +0,0 @@
-docker run -it \
-    --ipc=host \
-    --gpus all \
-    --env="XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \
-    --volume="$(pwd)/..:/home/maciek/workspace" \
-    --privileged \
-    --network=host \
-    --name=gps_denied_proj \
-    uav
