e6bb9976633719f32b4b61c95ae4597f45a32b13
omni <maciej.p.krupka@student.put.poznan.pl>
2025-07-05 20:00:47 +0200

    feat: added simple model and loading from hydra
    Signed-off-by: omni <maciej.p.krupka@student.put.poznan.pl>

--- models/simple.py
+++ models/simple.py
@@ -8,6 +8,57 @@ class SimpleCNN(nn.Module):
         self.conv2 = nn.Conv2d(D, 3, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(D)
 
+
+        def init_weights(m):
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, mean=0, std=0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+                
+        self.apply(init_weights)
+    
     def forward(self, img):
         x = self.relu(self.bn1(self.conv1(img)))
-        return self.conv2(x) * 0.01 + img
+        return self.conv2(x) + img
+    
+    
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, 3, 1, 1),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, channels, 3, 1, 1),
+            nn.BatchNorm2d(channels)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+class DeepCNN(nn.Module):
+    def __init__(self, D=64, num_blocks=4):
+        super().__init__()
+        self.entry = nn.Sequential(
+            nn.Conv2d(3, D, 3, 1, 1),
+            nn.BatchNorm2d(D),
+            nn.ReLU(inplace=True)
+        )
+        self.res_blocks = nn.Sequential(*[ResidualBlock(D) for _ in range(num_blocks)])
+        self.exit = nn.Conv2d(D, 3, 3, 1, 1)
+
+        self.apply(self.init_weights)
+
+    def init_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            nn.init.kaiming_normal_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, img):
+        x = self.entry(img)
+        x = self.res_blocks(x)
+        return self.exit(x) + img
