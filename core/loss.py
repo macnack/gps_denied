1081a1928d374cb9ccc3c1a8008861d63723c18f
omni <maciej.p.krupka@student.put.poznan.pl>
2025-07-05 20:01:10 +0200

    feat: added grad compute
    Signed-off-by: omni <maciej.p.krupka@student.put.poznan.pl>

--- core/loss.py
+++ core/loss.py
@@ -163,3 +163,11 @@ class CornerLoss(nn.Module):
         # # Normalize by batch size???
         # loss = loss / float(batch_size)
         return loss
+    
+def compute_grad_norm(model):
+    total_norm = 0.0
+    for p in model.parameters():
+        if p.grad is not None:
+            param_norm = p.grad.data.norm(2)
+            total_norm += param_norm.item() ** 2
+    return total_norm ** 0.5
